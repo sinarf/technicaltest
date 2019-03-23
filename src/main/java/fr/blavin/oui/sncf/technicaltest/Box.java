ef40fd42edf6fd65f9024d198da20ecd86305abd
Michel Blavin <michel@blavin.fr>
2019-03-23 10:50:00 +0100

    Added Box object

---
+++ src/main/java/fr/blavin/oui/sncf/technicaltest/Box.java
@@ -0,0 +1,62 @@
+package fr.blavin.oui.sncf.technicaltest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The Class Box, represents and standard box.
+ */
+public class Box {
+
+	/** The size of the box. */
+	protected static final int SIZE = 10;
+
+	/** List of contents in the box. */
+	private final List<Integer> contents = new ArrayList<Integer>();
+
+	/**
+	 * Checks if is full.
+	 *
+	 * @return true, if is full
+	 */
+	public boolean isFull() {
+		return SIZE == occupiedSpace();
+	}
+
+	/**
+	 * Adds the content. Return true if the content has been added, false otherwise.
+	 *
+	 * @param content the content
+	 * @return true, if successful
+	 */
+	public boolean addContent(int content) {
+		if (content > 0 && freeSpace() >= content) {
+			contents.add(content);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Occupied space.
+	 *
+	 * @return the occupied space in the box
+	 */
+	private int occupiedSpace() {
+		int occupiedSpace = 0;
+		for (Integer content : contents) {
+			occupiedSpace += content;
+		}
+		return occupiedSpace;
+	}
+
+	/**
+	 * Free space.
+	 *
+	 * @return the free space left in the box.
+	 */
+	public int freeSpace() {
+		return SIZE - occupiedSpace();
+	}
+
+}
---
+++ src/test/java/fr/blavin/oui/sncf/technicaltest/BoxTest.java
@@ -0,0 +1,55 @@
+package fr.blavin.oui.sncf.technicaltest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BoxTest {
+
+	@Test
+	public void testIsFull() throws Exception {
+		Box box = new Box();
+		box.addContent(10);
+		assertTrue("The box should be full", box.isFull());
+
+		box = new Box();
+		box.addContent(6);
+		box.addContent(4);
+		assertTrue("The box should be full", box.isFull());
+
+		box = new Box();
+		assertFalse("The box should NOT be full", box.isFull());
+
+		box = new Box();
+		box.addContent(9);
+		assertFalse("The box should NOT be full", box.isFull());
+		
+	}
+
+	@Test
+	public void testAddContent() throws Exception {
+		Box box = new Box();
+		assertTrue("The content should have been added.", box.addContent(10));
+		assertFalse("The box is full, the content shouldn't have been added", box.addContent(1));
+
+		box = new Box();
+		assertTrue("The content should have been added.", box.addContent(1));
+		assertTrue("The content should have been added.", box.addContent(9));
+		
+		box = new Box();
+		assertFalse("0 is not a valid content and shouldn't have been added", box.addContent(0));
+	}
+
+
+	@Test
+	public void testFreeSpace() throws Exception {
+		Box box = new Box();
+		assertEquals ("The box is empty, the free space should be the box size.", Box.SIZE, box.freeSpace());
+		box.addContent(2);
+		assertEquals (8, box.freeSpace());
+		box.addContent(8);
+	}
+
+}
